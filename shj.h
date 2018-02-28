54004cebb63a5a8aec24e62f77fc1259634a87cc
Sluts <30280873+Sluts@users.noreply.github.com>
2018-02-28 11:15:16 -0500

    Add files via upload

---
+++ shj.h
@@ -0,0 +1,16 @@
+struct Joe {
+private:
+  int hasHadCoffee;
+  int bored;
+  int coffees;
+  int performance;
+  
+public:
+  Joe();
+  void drinkCoffee();
+  void giveWork();
+  int getUse();
+  int getCoffeeCount();
+  int getBored();
+  void die();
+};
